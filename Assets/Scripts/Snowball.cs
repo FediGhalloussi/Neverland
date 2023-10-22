3e52c43b043555926b235440c91a6c7db68ca44c
Fedi Ghalloussi <62482405+FediGhalloussi@users.noreply.github.com>
2023-10-22 19:44:12 +0200

    Update Snowball.cs

--- Assets/Scripts/Snowball.cs
+++ Assets/Scripts/Snowball.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Snowball : MonoBehaviour
@@ -9,6 +10,16 @@ public class Snowball : MonoBehaviour
         {
             Instantiate(snowman, new Vector3(0, 0, 5f), Quaternion.identity);
             GameManager.Instance.GameSnowStarted = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Collision detected with " + other.collider.name);
+        if (!other.collider.CompareTag("Hand"))
+        {
+            Debug.Log("Collision detected with snowman");
+            Destroy(other.gameObject);
         }
     }
 }
