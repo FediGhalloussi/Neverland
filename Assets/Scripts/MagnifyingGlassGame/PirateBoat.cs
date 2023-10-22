6b95926295c5e4d1291cff14d3e4f7d79492dc8d
Loris <67026913+Toxysos@users.noreply.github.com>
2023-10-22 21:54:47 +0200

    pirate boat scene and asset

---
+++ Assets/Scripts/MagnifyingGlassGame/PirateBoat.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateBoat : MonoBehaviour
+{
+    [SerializeField] private Transform[] canonPoints;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
