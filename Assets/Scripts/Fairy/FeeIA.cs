f8e0aeb8a83a2743a76b88df7613ef494875d2a8
Loris <67026913+Toxysos@users.noreply.github.com>
2023-11-10 00:23:05 +0100

    adding fairy music

---
+++ Assets/Scripts/Fairy/FeeIA.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeeIA : MonoBehaviour
+{
+    public bool started = false;
+    private bool localStarted = false;
+    [SerializeField] private float initialSpeed;
+    private float speed;
+    [SerializeField] private float fearSpeed;
+    [SerializeField] private AnimationCurve speedCurve;
+    private Vector3 target;
+    private bool movementOver;
+    float movementDist;
+    float currentDist;
+    private Vector3[] handPositions;
+    private int currentPositionIndex;
+    [SerializeField] private GameObject hand;
+    private bool isScared;
+    [SerializeField] private float interactionDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        handPositions = new Vector3[10];
+        currentPositionIndex = 0;
+        speed = initialSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (started)
+        {
+            if (!localStarted)
+            {
+                StartMovement();
+                localStarted=true;
+                trackHandPosition();
+            }
+            Move();
+        }
+    }
+
+    private void StartMovement()
+    {
+        target = new Vector3(Random.Range(-.25f,.25f),Random.Range(-.25f,.25f),Random.Range(.90f,1.1f));
+        movementOver=false;
+        movementDist = Vector3.Distance(transform.position,target);
+    }
+
+    private void Move()
+    {
+        if (Vector3.Distance(transform.position,target)<speed*speedCurve.Evaluate(DistanceManagement())*Time.deltaTime)
+        {
+            if (!movementOver)
+            {
+                transform.position = target;
+                Invoke("StartMovement",0.15f);
+                movementOver=true;
+                isScared=false;
+                if (transform.position.z <2f) speed=initialSpeed;
+            }
+            
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed*speedCurve.Evaluate(DistanceManagement())*Time.deltaTime);
+            //print(DistanceManagement());
+            
+        }
+    }
+
+    private float DistanceManagement()
+    {
+        currentDist = Vector3.Distance(transform.position, target);
+        //print(currentDist/movementDist);
+        return currentDist/movementDist;
+    }
+
+    private void trackHandPosition()
+    {
+        handPositions[currentPositionIndex] = hand.transform.position;
+        if (currentPositionIndex<9) currentPositionIndex++;
+        else currentPositionIndex=0;
+        Invoke("trackHandPosition",0.2f);
+        if (Vector3.Distance(hand.transform.position,transform.position)<interactionDistance&&!isScared)
+        {
+            Scared();
+            isScared=true;
+            speed=fearSpeed;
+        }
+    }
+
+    private void Scared()
+    {
+        target = new Vector3(0f,0f,5f);
+        movementOver=false;
+        movementDist = Vector3.Distance(transform.position,target);
+    }
+}
