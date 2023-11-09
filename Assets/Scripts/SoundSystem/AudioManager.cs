1104dbfe78f0354a96212c8c8696a472d3766c36
Loris <67026913+Toxysos@users.noreply.github.com>
2023-11-09 22:20:52 +0100

    sound manager and audiomanager

---
+++ Assets/Scripts/SoundSystem/AudioManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+    }
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sounds => sounds.name == name);
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sounds => sounds.name == name);
+        s.source.Stop();
+    }
+}
