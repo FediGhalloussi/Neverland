243d937f19b6a68045078f5d076025a8d4d5c765
Loris <67026913+Toxysos@users.noreply.github.com>
2023-12-04 23:17:27 +0100

    last pirate scene fixes

--- Assets/Scripts/PirateGameActiver.cs
+++ Assets/Scripts/PirateGameActiver.cs
@@ -7,10 +7,12 @@ public class PirateGameActiver : MonoBehaviour
     [SerializeField] private GameObject part1;
 
     [SerializeField] private GameObject part2;
+    [SerializeField] private GameObject snow;
     
     public void Active1()
     {
         part1.SetActive(true);
+        snow.SetActive(false);
     }
 
     public void Active2()
